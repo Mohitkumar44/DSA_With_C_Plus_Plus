6bb49fcd2aa506ce81aa2e3535e24ee263bb241d
Mohit Kumar <mohit.kumar162534@gmail.com>
2025-07-22 16:49:01 +0530

    Lecture 2 Codes
    Lecture 2 practice codes from Apna College

--- Lecture 2 Pracitce/1st.cpp
+++ Lecture 2 Pracitce/1st.cpp
@@ -1,5 +1,14 @@
+// #include<iostream>
+// using namespace std;
+// int main(){
+//     cout << "Mohit Kumar" << endl;
+// }
+
 #include<iostream>
 using namespace std;
 int main(){
-    cout << "hello world" << endl;
+    int age = 34;
+    cout << "Mohit Kumar\nI am a Btech Student" << endl;
+    cout << age << endl;
+    cout << sizeof(age) << endl;
 }
---
+++ Lecture 2 Pracitce/Practice.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int a,b;
+    cout << "Enter the value of a : ";
+    cin >> a;
+    cout << "Enter the value of b : ";
+    cin >> b;
+    cout << "Sum = " << a+b << endl;
+    cout << "Subtraction is = " << a-b << endl;
+    cout << "Multiplication is = " << a*b << endl;
+    cout << "Division is = " << a/b << endl;
+    cout << "Remainder is = " << a%b << endl;
+    return 0;
+}
