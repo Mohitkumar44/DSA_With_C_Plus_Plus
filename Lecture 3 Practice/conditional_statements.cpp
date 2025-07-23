b48d84e32f94c1b193b759d506c8347a5e0bf79e
Mohit Kumar <mohit.kumar162534@gmail.com>
2025-07-23 15:46:27 +0530

    Conditional Statements

---
+++ Lecture 3 Practice/conditional_statements.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int n;
+    cin >> n;
+    if (n>=0)
+    {
+        cout << "You entered a positive number" << endl;
+    }
+    else{
+        cout << "You entered a negative number" << endl;
+    }
+    
+    return 0;
+}
