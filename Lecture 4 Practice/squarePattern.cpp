20f122dfe150c93dfcd5a58907bc5607b7d666f4
Mohit Kumar <mohit.kumar162534@gmail.com>
2025-07-25 01:00:57 +0530

    Patterns

--- Lecture 4 Practice/squarePattern.cpp
+++ Lecture 4 Practice/squarePattern.cpp
@@ -1,18 +1,115 @@
 #include<iostream>
 using namespace std;
 int main(){
-    int n=10;
-    for (int i = 1; i <= n; i++)
-    {
-        char ch = 'A';
-        for (int j = 1; j <= n; j++)
-        {
-            cout << ch;
-            ch = ch+1;
-        }
-        cout << endl;
+    int n=5;
+
+
+
+
+
+
+    // for (int i = 1; i <= n; i++)
+    // {
+    //     char ch = 'A';
+    //     for (int j = 1; j <= n; j++)
+    //     {
+    //         cout << ch;
+    //         ch = ch+1;
+    //     }
+    //     cout << endl;
         
-    }
+    // }
+
+
+
+
+
+
+    // char ch='A';
+    // for (int i = 0; i < n; i++)
+    // {
+    //     for (int i = 0; i < n; i++)
+    //     {
+    //         cout << ch << " ";
+    //         ch++;
+    //     }
+    //     cout <<endl;
+
+    // }
+
+
+    // char ch='A';
+    // for (int i = 0; i < n; i++)
+    // {
+    //     for (int j = 0; j < i+1; j++)
+    //     {
+    //         cout << ch << " ";
+    //     }
+    //     cout << endl;
+    //     ch++;
+    // }
     
-    return 0;
-}
+    
+    
+    // for (int i = 0; i < n; i++)
+    // {
+        //     for (int j = i+1; j > 0; j--)
+        //     {
+            //         cout << j << " ";
+            //     }
+            //     cout << endl;
+            // }
+            
+
+
+            
+            // for (int i = 0; i < n; i++)
+            // {
+            //     char ch='A';
+            //     for (int j = i+1; j >0; j--)
+            //     {
+            //         char temp = ch+j-1;
+            //         cout << temp << " ";
+            //     }
+            //     cout << endl;
+            //     ch++;
+            // }
+
+
+
+            
+            // char ch='A';
+            // for (int i = 0; i < n; i++)
+            // {
+            //     for (int j = i+1; j >0; j--)
+            //     {
+            //         cout << ch << " ";
+            //         ch++;
+            //     }
+            //     cout << endl;
+            // }
+            
+
+
+            
+            char ch='A';
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < i; j++)
+                {
+                    cout << " ";
+                }
+                
+                for (int j = n-i; j >0; j--)
+                {
+                    cout << ch;
+                }
+                cout << endl;
+                ch++;
+            }
+            
+            
+            
+            
+            return 0;
+        }
