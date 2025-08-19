3549f3b1668ef9f4d2f8d24bbafb9eaba5f14714
Mohit Kumar <mohit.kumar162534@gmail.com>
2025-08-19 14:01:51 +0530

    Practice

---
+++ Vector/vector.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+int main(){
+    vector<int> vec = {1,2,3,4,5,6,7,8,9,0};
+    vector<char> vec1 = {'a','b','c','d','e','f'};
+    for(int i : vec){
+        cout << i << " ";
+    }
+    for(char i : vec1){
+        cout << i << " ";
+    }
+    cout << vec.size() << endl;
+    return 0;3
+}
