a46e65803ffe10ad41c9e0bf1b9cc9718be376f1
Mohit Kumar <mohit.kumar162534@gmail.com>
2025-09-19 03:39:41 +0530

    Solve

--- Pattern2/numberPyramidPallindrome.cpp
+++ Pattern2/numberPyramidPallindrome.cpp
@@ -15,9 +15,13 @@ int main()
         {
             cout <<j;
         }
-        for (int j = 1; j < i; j++)
+        // for (int j = 1; j < i; j++)
+        // {
+        //     cout<<i-j;
+        // }
+        for (int j = i-1; j >= 1; j--)
         {
-            cout<<i-j;
+            cout<<j;
         }
         
         cout << endl; // for next line
