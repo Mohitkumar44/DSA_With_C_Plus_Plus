03b490f805dc7fa1fb2bc312f5f2afa4452eb31d
Mohit Kumar <mohit.kumar162534@gmail.com>
2025-09-26 00:05:44 +0530

    input through pointers

--- Pointers/sumOfTwo.cpp
+++ Pointers/sumOfTwo.cpp
@@ -1,9 +1,14 @@
 #include<iostream>
 using namespace std;
 int main(){
-    int n1 = 10,n2=20;
+    // int n1 = 10,n2=20;
+    int n1,n2;
     int* p1=&n1;
     int* p2=&n2;
+    cout<<"Enter first number : ";
+    cin>>*p1;
+    cout<<"Enter second number : ";
+    cin>>*p2;
     int sum = *p1+*p2;
     cout<<sum;
     return 0;
