29478befc8de5c33142c7bd3fca165b303d560b8
Mohit Kumar <mohit.kumar162534@gmail.com>
2025-10-04 00:07:09 +0530

    move negative

---
+++ array3/moveNegative.cpp
@@ -0,0 +1,46 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+void display(vector<int>& v){
+    for (int i = 0; i < v.size(); i++)
+    {
+        cout<<v[i]<<" ";
+    }
+    cout<<endl;
+}
+void megaSortSwap(vector<int>& v){
+    int i=0,j=v.size()-1;
+    while(i<j){
+        if (v[i]>0&&v[j]<0)
+        {
+            int temp=v[i];
+            v[i]=v[j];
+            v[j]=temp;
+            i++;
+            j--;
+        }
+        else if (v[i]<0)
+        {
+            i++;
+        }
+        
+        else{
+            j--;
+        }
+    }    
+}
+int main(){
+    vector<int> v;
+    v.push_back(1);
+    v.push_back(-2);
+    v.push_back(3);
+    v.push_back(-4);
+    v.push_back(5);
+    v.push_back(-6);
+    v.push_back(7);
+    v.push_back(8);
+    display(v);
+    megaSortSwap(v);
+    display(v);
+    return 0;
+}
