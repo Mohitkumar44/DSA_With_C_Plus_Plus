876d8c872d788648b9be1ab9f720e4c9cfbb05c6
Mohit Kumar <mohit.kumar162534@gmail.com>
2025-10-04 14:33:49 +0530

    format

--- array3/mergeTwoSortedArray.cpp
+++ array3/mergeTwoSortedArray.cpp
@@ -1,62 +1,68 @@
-#include<iostream>
-#include<vector>
+#include <iostream>
+#include <vector>
 using namespace std;
-void display(vector<int>& v){
+void display(vector<int> &v)
+{
     for (int i = 0; i < v.size(); i++)
     {
-        cout<<v[i]<<" ";
+        cout << v[i] << " ";
     }
-    cout<<endl;
+    cout << endl;
 }
-void merge(vector<int>& v1,vector<int>& v2,vector<int>& v3){
-    int i = 0,j=0,k=0,m=v1.size(),n=v2.size();
-    while (i < m && j < n )
+void merge(vector<int> &v1, vector<int> &v2, vector<int> &v3)
+{
+    int i = 0, j = 0, k = 0, m = v1.size(), n = v2.size();
+    while (i < m && j < n)
     {
-        if (v1[i]<=v2[j])
+        if (v1[i] <= v2[j])
         {
             v3.push_back(v1[i]);
             i++;
         }
-        else{
-            v3.push_back(v2[j]);
-            j++;        
-        }
-    }    
-    if (i==m)
-    {
-        while (j<n)
+        else
         {
             v3.push_back(v2[j]);
             j++;
-            k++;   
         }
     }
-    else if (j==n)
+    if (i == m)
     {
-        while (i<m)
+        while (j < n)
+        {
+            v3.push_back(v2[j]);
+            j++;
+            k++;
+        }
+    }
+    else if (j == n)
+    {
+        while (i < m)
         {
             v3.push_back(v1[i]);
             i++;
-            k++;   
+            k++;
         }
     }
-    
 }
-int main(){
+int main()
+{
     vector<int> v1;
-    vector<int> v2;
-    vector<int> v3;
     v1.push_back(1);
     v1.push_back(2);
     v1.push_back(4);
     v1.push_back(6);
+
+    vector<int> v2;
     v2.push_back(2);
     v2.push_back(3);
     v2.push_back(4);
     v2.push_back(5);
+
+    vector<int> v3;
+
     display(v1);
     display(v2);
-    merge(v1,v2,v3);
+    merge(v1, v2, v3);
     display(v3);
 
     return 0;
