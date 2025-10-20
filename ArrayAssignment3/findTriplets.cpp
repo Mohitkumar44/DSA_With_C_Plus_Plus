fb341901d22aee542a0a305dadde97d3673e1e75
Mohit Kumar <mohit.kumar162534@gmail.com>
2025-10-21 00:12:34 +0530

    Triplets Optimizing

--- ArrayAssignment3/findTriplets.cpp
+++ ArrayAssignment3/findTriplets.cpp
@@ -1,12 +1,7 @@
 #include<iostream>
 using namespace std;
-int main(){
-    int arr[] = {1, 3, 5, 7, 9};
-    int n = sizeof(arr)/sizeof(arr[0]);
-    int x;
+int triplets(int arr[],int n,int x){
     int count = 0;
-    cout<<"Enter the value of X : ";
-    cin>>x;
     for (int i = 0; i < n; i++)
     {
         for (int j = i+1; j < n; j++)
@@ -21,6 +16,22 @@ int main(){
         }
         
     }
-    cout<<count;
+    return count;
+}
+int main(){
+    int n;
+    cout<<"Enter no. of elements : ";
+    cin>>n;
+    int arr[n];
+    for (int i = 0; i < n; i++)
+    {
+        cin>>arr[i];
+    }
+    
+    int x;
+    cout<<"Enter the value of X : ";
+    cin>>x;
+    
+    cout<<triplets(arr,n,x);
     return 0;
 }
