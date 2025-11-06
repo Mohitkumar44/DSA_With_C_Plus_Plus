bba7b566ba47b1e716027e3257920bc893dc6ea8
Mohit Kumar <mohit.kumar162534@gmail.com>
2025-11-07 00:47:09 +0530

    temp

---
+++ 2dArraysAssignments/assignment2/spiralFillMatrix.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int n;
+    cout<<"Enter the value of n : ";
+    cin>>n;
+    int arr[n][n];
+    int ri=0,rl=n,ci=0,cl=n;
+
+    return 0;
+}
