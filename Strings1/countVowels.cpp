a96043ff48bdaeb9ecc68987e873b894df3f51a2
Mohit Kumar <mohit.kumar162534@gmail.com>
2026-01-31 11:29:40 +0530

    Strings

--- Strings1/countVowels.cpp
+++ Strings1/countVowels.cpp
@@ -3,51 +3,15 @@
 using namespace std;
 int main(){
     string str;
-    int a = 0, b = 0, c = 0, d = 0, e = 0;
-    int A = 0, B = 0, C = 0, D = 0, E = 0;
     cin>>str;
-    for(int i = 0; str[i]!='\0'; i++){
-        if(str[i]=='a'){
-            a++;
+    int count = 0;
+    for(int i= 0; str[i]!='\0'; i++){
+        if (str[i]=='a' || str[i]=='e' || str[i]=='i' || str[i]=='o' || str[i]=='u' || str[i]=='A' || str[i]=='E' || str[i]=='I' || str[i]=='O' || str[i]=='U')
+        {
+            count++;
         }
-        else if(str[i]=='b'){
-            b++;
-        }
-        else if(str[i]=='c'){
-            c++;
-        }
-        else if(str[i]=='d'){
-            d++;
-        }
-        else if(str[i]=='e'){
-            e++;
-        }
-        else if(str[i]=='A'){
-            A++;
-        }
-        else if(str[i]=='B'){
-            B++;
-        }
-        else if(str[i]=='C'){
-            C++;
-        }
-        else if(str[i]=='D'){
-            D++;
-        }
-        else if(str[i]=='E'){
-            E++;
-        }
+        
     }
-
-    cout<<"a = "<<a<<endl;
-    cout<<"b = "<<b<<endl;
-    cout<<"c = "<<c<<endl;
-    cout<<"d = "<<d<<endl;
-    cout<<"e = "<<e<<endl;
-    cout<<"A = "<<A<<endl;
-    cout<<"B = "<<B<<endl;
-    cout<<"C = "<<C<<endl;
-    cout<<"D = "<<D<<endl;
-    cout<<"E = "<<E<<endl;
+    cout<<"no. of vowels = "<<count<<endl;
     return 0;
 }
