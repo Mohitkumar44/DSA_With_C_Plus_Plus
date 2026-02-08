035793be1bd56ce996a815a28160d627ca388e07
Mohit Kumar <mohit.kumar162534@gmail.com>
2026-02-08 10:13:08 +0530

    old sync

--- string2/mostFrequencyWord.cpp
+++ string2/mostFrequencyWord.cpp
@@ -1,17 +1,48 @@
 #include<iostream>
 #include<string>
 #include<sstream>
+#include<vector>
+#include<algorithm>
 
 using namespace std;
 
 int main(){
     string s = "Raghav is a maths teacher. He is a DSA mentor as well.";
     string temp;
-    stringstream ss;
+    stringstream ss(s);
     vector<string> v;
     while(ss>>temp){
-        v.push_back();
+        v.push_back(temp);
     }
     
-    return;
+    sort(v.begin(),v.end());
+
+    int maxCount = 0;
+    string ans = v[0];
+    int count = 1;
+    for (int i = 0; i < v.size()-1; i++)
+    {
+        if(v[i]==v[i+1]){
+            count++;
+        }
+        else{
+            count = 1;
+        }
+        maxCount = max(maxCount,count);
+    }
+    count = 1;
+    for (int i = 0; i < v.size()-1; i++)
+    {
+        if(v[i]==v[i+1]){
+            count++;
+        }
+        else{
+            count = 1;
+        }
+        if(count==maxCount){
+            cout<<v[i]<<" "<<maxCount<<endl;
+        }
+    }
+
+    return 0;
 }
