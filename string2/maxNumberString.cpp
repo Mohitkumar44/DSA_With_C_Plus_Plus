c38ce1a51e81d4de91b04774d2235e3e2b0ad038
Mohit Kumar <mohit.kumar162534@gmail.com>
2026-02-08 16:40:56 +0530

    input change

--- string2/maxNumberString.cpp
+++ string2/maxNumberString.cpp
@@ -3,9 +3,14 @@
 
 using namespace std;
 int main(){
-    vector<string> v = {"0123","0023","456","00182","940","2901"};
+    int n;
+    cout<<"Enter the value of n : ";
+    cin>>n;
+    vector<string> v(n);
+    for(int i = 0; i < n; i++){
+        cin>>v[i];
+    }
     vector<int> v1;
-    int n = v.size();
     for (int i = 0; i < n; i++)
     {
         v1.push_back(stoi(v[i]));
