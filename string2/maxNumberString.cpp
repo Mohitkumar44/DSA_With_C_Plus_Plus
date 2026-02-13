ad177f1db284605498f0b03511656d524e7ef4cb
Mohit Kumar <mohit.kumar162534@gmail.com>
2026-02-13 18:14:17 +0530

    maxNumberString

--- string2/maxNumberString.cpp
+++ string2/maxNumberString.cpp
@@ -10,24 +10,22 @@ int main(){
     for(int i = 0; i < n; i++){
         cin>>v[i];
     }
-    vector<int> v1;
-    for (int i = 0; i < n; i++)
-    {
-        v1.push_back(stoi(v[i]));
-    }
+    // vector<int> v1;
+    // for (int i = 0; i < n; i++)
+    // {
+    //     v1.push_back(stoi(v[i]));
+    // }
     int idx = 0;
-    int max = v1[0];
-    for (int i = 1; i < n; i++)
-    {
-        if (max<v1[i])
-        {
+    int max = stoi(v[0]);
+    for (int i = 1; i < n; i++){
+        if (max<stoi(v[i])){
             idx = i;
-            max = v1[i];
+            max = stoi(v[i]);
         }
         
     }
-
     cout<<"Max number's index : "<<idx<<endl;
+    cout<<"Max number is : "<<v[idx]<<endl;
     
     return 0;
 }
