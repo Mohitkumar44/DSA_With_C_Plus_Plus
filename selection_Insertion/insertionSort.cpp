56b2a2b2753c2c8fd15df0f0af112539d251b902
Mohit Kumar <mohit.kumar162534@gmail.com>
2026-02-19 14:28:12 +0530

    formatting the code

--- selection_Insertion/insertionSort.cpp
+++ selection_Insertion/insertionSort.cpp
@@ -4,32 +4,23 @@ int main(){
     int n = 5;
     int arr[] = {3,2,1,4,5};
     
-    for (int i = 0; i < n; i++)
-    {
+    for (int i = 0; i < n; i++){
         cout<<arr[i]<<" ";
     }
     cout<<endl;
     
-    for (int i = 1; i < n; i++)
-    {
-        for (int j = i; j >= 1; j--)
-        {
-            if (arr[j]>=arr[j-1])
-            {
-                break;
-            }
-            else{
-                swap(arr[j],arr[j-1]);
-            }
-            
+    for (int i = 1; i < n; i++){
+        for (int j = i; j >= 1; j--){
+            if (arr[j]>=arr[j-1]) break;
+            else swap(arr[j],arr[j-1]);            
         }
         
     }
     
-    for (int i = 0; i < n; i++)
-    {
+    for (int i = 0; i < n; i++){
         cout<<arr[i]<<" ";
     }
     cout<<endl;
+    
     return 0;
 }
