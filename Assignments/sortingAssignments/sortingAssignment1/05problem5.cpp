b9c077cc340055c2cc9c23b075f1497e786e360a
Mohit Kumar <mohit.kumar162534@gmail.com>
2026-03-13 18:50:36 +0530

    need correction

--- Assignments/sortingAssignments/sortingAssignment1/05problem5.cpp
+++ Assignments/sortingAssignments/sortingAssignment1/05problem5.cpp
@@ -1,22 +1,22 @@
 // Check if the given array is almost sorted. (elements are at-most one position away).
+                        // this needs correction
+// #include<iostream>
+// using namespace std;
+// int main(){
+//     int arr[] = {2, 1, 4, 3, 6, 5};
+//     int n = 6;
 
-#include<iostream>
-using namespace std;
-int main(){
-    int arr[] = {1,2,3,5,6,4};
-    int n = 6;
-
-    int c = 0;
-    for (int j = 0; j < n-1; j++)
-    {
-        bool f = true;
-        if(arr[j]>arr[j+1]){
-            swap(arr[j],arr[j+1]);
-            c++;
-        }
-    }
-    if(c==1) cout<<"array was almost sorted"<<endl;
-    else cout<<"array was not almost sorted"<<endl;
+//     int c = 0;
+//     for (int j = 0; j < n-1; j++)
+//     {
+//         bool f = true;
+//         if(arr[j]>arr[j+1]){
+//             swap(arr[j],arr[j+1]);
+//             c++;
+//         }
+//     }
+//     if(c==1) cout<<"array was almost sorted"<<endl;
+//     else cout<<"array was not almost sorted"<<endl;
        
-    return 0;
-}
+//     return 0;
+// }
