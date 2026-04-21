267c485eebd67ac4263a5f81d269fffbffd24fc8
Mohit Kumar <mohit.kumar162534@gmail.com>
2026-04-21 10:26:49 +0530

    towerOfHanoi

--- recursion3/towerOfHanoi.cpp
+++ recursion3/towerOfHanoi.cpp
@@ -1,14 +1,22 @@
 #include<iostream>
 using namespace std;
-int towerOfHanoi(int n){
-    if(n==1) return 1;
-    if(n==2) return 3;
-    return 1 + 2*towerOfHanoi(n-1);
+// int towerOfHanoi(int n){
+//     if(n==1) return 1;
+//     if(n==2) return 3;
+//     return 1 + 2*towerOfHanoi(n-1);
+// }
+
+void towerOfHanoi(int n, char s, char h, char d){
+    if (n==0) return;
+    towerOfHanoi(n-1,s,d,h);
+    cout<<s<<" --> "<<d<<endl;
+    towerOfHanoi(n-1,h,s,d);
 }
+
 int main(){
     int n;
     cout<<"Enter n : ";
     cin>>n;
-    cout<<towerOfHanoi(n);
+    towerOfHanoi(n,'A','B','C');
     return 0;
 }
