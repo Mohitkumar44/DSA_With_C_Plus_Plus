e2e86c9b3738bc8f954c4956e3b666b434e97c04
Mohit Kumar <mohit.kumar162534@gmail.com>
2026-06-14 20:51:19 +0530

    printFactors

---
+++ mathsAndPrimeNumbers/printFactors.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+#include<cmath>
+using namespace std;
+void printFactors(int n){
+    for(int i = 1; i < sqrt(n); i++){
+        if(n%i==0) cout<<i<<" ";        
+    }
+    for(int i = sqrt(n); i >= 1; i--){
+        if(n%i==0) cout<<n/i<<" ";        
+    }
+}
+int main(){
+    int n = 60;
+    printFactors(n);    
+    return 0;
+}
